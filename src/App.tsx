76df98ef3190308cb489df13a0212596faa0d22f
Wisesa <hi@wisesa.dev>
2024-09-24 19:49:19 +0700

    feat: service worker

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { useEffect } from "react";
+import { getSerwist } from "virtual:serwist";
 import StationList from "./components/station-list";
 import Navbar from "./components/navbar";
 import { StationProvider } from "./contexts/stations-context";
 
 function App() {
+  useEffect(() => {
+    const loadSerwist = async () => {
+      if ("serviceWorker" in navigator) {
+        const serwist = await getSerwist();
+
+        serwist?.addEventListener("installed", () => {
+          console.log("Serwist installed!");
+        });
+
+        void serwist?.register();
+      }
+    };
+
+    loadSerwist();
+  }, []);
   return (
     <StationProvider>
       <Navbar />
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { serwist } from "@serwist/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    serwist({
+      swSrc: "src/sw.ts",
+      swDest: "sw.js",
+      globDirectory: "dist",
+      injectionPoint: "self.__SW_MANIFEST",
+      rollupFormat: "iife",
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
