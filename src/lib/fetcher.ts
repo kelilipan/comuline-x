a4f9782d205bd73b4fa01a8d47a53342c70481fe
Wisesa <hi@wisesa.dev>
2024-09-26 15:08:25 +0700

    chore: remove proxy

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -2,9 +2,6 @@ export default async function fetcher<JSON = any>(
   input: RequestInfo,
   init?: RequestInit
 ): Promise<JSON> {
-  const proxyURL = import.meta.env.VITE_PROXY_URL;
-  const prevURL = typeof input === "string" ? input : input.url;
-  const newURL = proxyURL ? new URL(proxyURL + prevURL, proxyURL) : input;
-  const res = await fetch(newURL, init);
+  const res = await fetch(input, init);
   return res.json();
 }
